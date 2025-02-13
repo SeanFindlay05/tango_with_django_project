af0f030d817731acddd46fb5e10c628b5da3993b
“Sean <2897370F@student.gla.ac.uk>
2025-02-13 21:24:24 +0000

    Chapter 9-No tests flies left in(FIX)

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,10 +6,9 @@ from rango.forms import CategoryForm
 from django.shortcuts import redirect
 from rango.forms import PageForm
 from django.urls import reverse
-
-# def index(request):
-#     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-#     return render(request, 'rango/index.html', context=context_dict)
+from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -43,7 +42,7 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context=context_dict)
 
-
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -51,7 +50,6 @@ def add_category(request):
         form = CategoryForm(request.POST)
 
         if form.is_valid():
-            # Save the new category to the database.
             form.save(commit=True)
             return redirect('/rango/')
     else:
@@ -59,14 +57,13 @@ def add_category(request):
 
     return render(request, 'rango/add_category.html', {'form': form})
 
-
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
     except Category.DoesNotExist:
             category = None
 
-    # You cannot add a page to a Category that does not exist...
     if category is None:
         return redirect('/rango/')
     
@@ -88,3 +85,68 @@ def add_page(request, category_name_slug):
 
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict) 
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'rango/register.html',
+                    context = {'user_form': user_form,
+                                'profile_form': profile_form,
+                                'registered': registered})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+        
+    else:
+        return render(request, 'rango/login.html')
+    
+@login_required
+def restricted(request):
+        return render(request, 'rango/restricted.html', context={})
+
+# Use the login_required() decorator to ensure only those logged in can # access the view.
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+    # Take the user back to the homepage.
+    return redirect(reverse('rango:index'))
